0c06e2eb78fbdb44b37e4877228ddfac0d9a4e21
mahikero <federicoparassarmiento@yahoo.com>
2015-12-04 07:09:04 -0500

    up

---
+++ CS-ASP/ChallengeDaysBetweenDates.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP
+{
+    public partial class ChallengeDaysBetweenDates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime startDate = firstCalendar.SelectedDate;
+            DateTime finishDate = secondCalendar.SelectedDate;
+            TimeSpan numberOfDays;
+
+            if (finishDate > startDate)
+            {
+                numberOfDays = finishDate.Subtract(startDate);
+                resultLabel.Text = numberOfDays.TotalDays.ToString();
+            }
+            else
+            {
+                numberOfDays = startDate.Subtract(finishDate);
+                resultLabel.Text = numberOfDays.TotalDays.ToString();
+            }
+            
+        }
+    }
+}
